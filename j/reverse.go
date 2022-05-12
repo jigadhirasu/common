edad42c6985016c99b5d98c2394a2ad146fae0b0
Hao <hao@glowwormonline.com>
2022-05-12 13:48:47 +0800

    j

---
+++ j/reverse.go
@@ -0,0 +1,16 @@
+package j
+
+import "reflect"
+
+func Reverse(slice interface{}) interface{} {
+	v := reflect.ValueOf(slice)
+	if v.Kind() != reflect.Slice {
+		return slice
+	}
+
+	ns := reflect.Indirect(reflect.New(v.Type()))
+	for i := v.Len(); i > 0; i-- {
+		ns = reflect.Append(ns, v.Index(i-1))
+	}
+	return ns.Interface()
+}
